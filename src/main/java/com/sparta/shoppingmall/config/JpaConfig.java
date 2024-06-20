a01663cd5db49ffd313c33486dffe3b02be040ad
yoonjae <backendINFJ@github.com>
2024-06-20 10:16:53 +0900

    commit test

--- src/main/java/com/sparta/shoppingmall/config/JpaConfig.java
+++ src/main/java/com/sparta/shoppingmall/config/JpaConfig.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration // 아래 설정을 등록하여 활성화
 @EnableJpaAuditing // 시간 자동으로 변경 가능하도록 함.
-public class JpaConfig {
+public class JpaConfig { // git commit test
 }
