66e2d8dd01d965a4a89ee785f841008c9b9ebfe2
yoonjae <backendINFJ@github.com>
2024-06-21 21:23:36 +0900

    Feat : WithdrawRequestDTO 구현

---
+++ src/main/java/com/sparta/shoppingmall/user/dto/WithdrawRequestDTO.java
@@ -0,0 +1,13 @@
+package com.sparta.shoppingmall.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class WithdrawRequestDTO {
+
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String password;
+
+}
