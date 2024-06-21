4e80c061bf5adec7a63c48209e923226630bfc87
yoonjae <backendINFJ@github.com>
2024-06-21 21:14:41 +0900

    Feat : UserException 구현

---
+++ src/main/java/com/sparta/shoppingmall/user/exception/UserException.java
@@ -0,0 +1,7 @@
+package com.sparta.shoppingmall.user.exception;
+
+public class UserException extends IllegalArgumentException {
+    public UserException(String message) {
+        super(message);
+    }
+}
